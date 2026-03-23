c07fef7d3e71f8b84ab3d65f30c2db3f4998a7f0
Takayuki Miyoshi <takayukister@gmail.com>
2026-03-23 16:33:28 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+/**
+ * Converts a multi-layered object to a flat array.
+ */
+export const flatten = tree => {
+	const array = [];
+
+	if ( Object === tree.constructor ) {
+		for ( const value of Object.values( tree ) ) {
+			array.push( ...flatten( value ) );
+		}
+	} else if ( tree instanceof Map ) {
+		for ( const [ key, value ] of tree ) {
+			array.push( ...flatten( value ) );
+		}
+	} else if ( Array.isArray( tree ) ) {
+		for ( const value of tree ) {
+			array.push( ...flatten( value ) );
+		}
+	} else {
+		array.push( tree );
+	}
+
+	return array;
+};
